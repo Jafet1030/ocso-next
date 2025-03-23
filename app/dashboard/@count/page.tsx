57c1c4792b140e0bbed7bdbbfd71d05a857954b2
Jafet1030 <eibarra24@alumnos.uaq.mx>
2025-03-22 21:54:57 -0600

    feat: Cookies in SSC

--- app/dashboard/@count/page.tsx
+++ app/dashboard/@count/page.tsx
@@ -1,7 +1,16 @@
-import axios from "axios";
-const CountPage = async () => {
-    const countLocations = await axios.get("http://127.0.0.1:4000/locations");
-    return "hay tantas locations: " + countLocations?.data?.length;
-}
-
-export default CountPage;
+import { TOKEN_NAME } from "@/constants";
+ import axios from "axios";
+ import { cookies } from "next/headers";
+ const CountPage = async () => {
+   const userCookies = cookies()
+   const token = userCookies.get(TOKEN_NAME)?.value
+   const countLocations = await axios.get("http://127.0.0.1:4000/locations", {
+     headers: {
+       Authorization: `Bearer ${token}`
+     }
+   })
+   const cantidad = countLocations?.data?.length;
+   return `Hay: ${cantidad} tienda${cantidad > 1 ? "s" : ""}`
+ }
+ 
+ export default CountPage;
