3a3824fb07371f014321cbdbbf9d665517d1d2c6
Jafet1030 <eibarra24@alumnos.uaq.mx>
2025-03-22 19:26:21 -0600

    feat: JWT retrieved

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+export default function DashboardPage() {
+    return (
+        <>
+            <div className="h-full w-1/2 bg-red-100">
+                <p>Welcome to the dashboard!</p>
+            </div>
+            <div className="h-full w-2/2 bg-red-100">
+                <p>Welcome to the dashboard!</p>
+            </div>
+        </>
+    );
+}
