f513567c67d90c59a85cf17037c9be4938f82b2f
Jafet1030 <eibarra24@alumnos.uaq.mx>
2025-03-22 22:43:54 -0600

    feat: Implementation Location

--- app/dashboard/@locations/page.tsx
+++ app/dashboard/@locations/page.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
  import { Location } from "@/entities";
  import { TOKEN_NAME } from "@/constants";
 import SelectLocation from "./_components/SelectLocation";
+import LocationCard from "./_components/LocationCard";
  const LocationsPage = async ({searchParams}: {
    searchParams: { [key: string]: string | string[] | undefined };
  }) => {
@@ -23,13 +24,16 @@ import SelectLocation from "./_components/SelectLocation";
        locationLatLng: [0,0],
        locationAddress: "No existe"
      },
-     ...data
+     ...data,
    ]
    return (
      <div className="w-7/12">
        <div className="w-full flex flex-col items-center h-[90vh] bg-red-50">
          <div className="w-1/2 my-10">
            <SelectLocation locations={data} store={ searchParams.store  } />
+         </div>
+         <div className="w-8/12">
+            <LocationCard store={searchParams.store}/>
          </div>
        </div>
      </div>
